f34ae71e4b99c38f0162e54f202c61a3d303764b
kaustubh singh <kaustubhsingh@kaustubhs-MacBook-Air.local>
2026-02-22 10:10:59 +0530

    added comments in genomeGenerator.hpp

--- include/genomeGenerator.hpp
+++ include/genomeGenerator.hpp
@@ -7,11 +7,16 @@
 #include <random>
 #include <ctime>
 
+/**
+ * @struct BaseInfo
+ * @brief conatins information regard position of base in the sequence as well as base type amongst [A,T,G,C]
+ */
+
 struct BaseInfo {
- 
+
     char        base;
     size_t      position;
-}; // maybe inherit from RegionInfo struct from "regionGenerator.hpp".
+};
 
 class GenomeGenerator {
 private:
